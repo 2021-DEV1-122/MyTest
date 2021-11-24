a494de936b874bcdb264d53bc6c0cc6b3b370e94
2021dev1122 <2021dev1122@email>
2021-11-24 15:28:58 +0100

    [3][InputText] Create Input Text component and fix test

---
+++ src/components/InputText/InputText.jsx
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+export  default class InputText extends Component {
+
+    render() {
+        return (
+            <div>
+            </div>
+        )
+    }
+}
+
