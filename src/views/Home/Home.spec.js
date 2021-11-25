cd1859d9c0179509264b05c725187dd9d5676acd
2021dev1122 <2021dev1122@email>
2021-11-25 22:12:14 +0100

    [8][Home Page] Create test : should render a Two input with button submit

---
+++ src/views/Home/Home.spec.js
@@ -0,0 +1,16 @@
+import React, {useState as useStateMock} from 'react';
+import {shallow} from 'enzyme';
+import Home from "./Home";
+
+describe('<Home>', () => {
+    let wrapper;
+    beforeEach(() => {
+        wrapper = shallow(<Home/>)
+    });
+
+    it('should render a Two input with button submit', () => {
+        expect(wrapper.find('InputText').length).toEqual(2);
+        expect(wrapper.find('button').length).toEqual(1);
+    });
+
+});
