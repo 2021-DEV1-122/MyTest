0006d1e5c28872e98dc0e41b012544a4790da91b
2021dev1122 <2021dev1122@email>
2021-11-25 19:03:09 +0100

    [5][InputText] Create Test for capture input Value correctly onChange

--- src/components/InputText/InputText.spec.js
+++ src/components/InputText/InputText.spec.js
@@ -1,14 +1,42 @@
-import React from 'react';
-import { shallow } from 'enzyme';
+import React, { useState as useStateMock } from 'react';
+import {shallow} from 'enzyme';
+
 import InputText from "./InputText";
+jest.mock('react', () => ({
+    ...jest.requireActual('react'),
+    useState: jest.fn(),
+}));
+
 
 describe('InputText', () => {
-    let container;
+    let wrapper;
+    const setState = jest.fn();
 
-    beforeEach(() => (container = shallow(<InputText />)));
+
+    beforeEach(() => {
+        useStateMock.mockImplementation(init => [init, setState]);
+        wrapper = shallow(<InputText/>)
+    });
+
+    afterEach(() => {
+        jest.clearAllMocks();
+    });
+
 
     it('should render a <div />', () => {
-        expect(container.find('div').length).toEqual(1);
+        expect(wrapper.find('div').length).toEqual(1);
+    });
+
+    describe("input Text", () => {
+        it("Should capture input value correctly onChange", () => {
+            const inputValue = wrapper.find("input").at(0);
+            inputValue.simulate('change', {
+                target: {
+                    value: 'Test'
+                }
+            });
+            expect(setState).toHaveBeenCalledWith("Test");
+        });
     });
 
 });
