82fa093926fea574fc6296b8b9068f5b5c317708
2021dev1122 <2021dev1122@email>
2021-11-25 21:11:20 +0100

    [7][InputText] Create test : Renders a Input with id of "id1" and placeholder message of "Enter your userName" and  title message of "User One"

--- src/components/InputText/InputText.spec.js
+++ src/components/InputText/InputText.spec.js
@@ -1,42 +1,52 @@
-import React, { useState as useStateMock } from 'react';
+import React, {useState as useStateMock} from 'react';
 import {shallow} from 'enzyme';
 
 import InputText from "./InputText";
+
 jest.mock('react', () => ({
     ...jest.requireActual('react'),
     useState: jest.fn(),
 }));
+const props = {
+    id: '',
+    placeholder: '',
+    title: ''
+};
 
-
-describe('InputText', () => {
+describe('<InputText id="id1" placeholder="Enter your userName" title="User One" />', () => {
     let wrapper;
     const setState = jest.fn();
-
+    let inputValue;
 
     beforeEach(() => {
         useStateMock.mockImplementation(init => [init, setState]);
-        wrapper = shallow(<InputText/>)
+        wrapper = shallow(<InputText {...props} id="id1" placeholder="Enter your userName" title="User One"/>)
+        inputValue = wrapper.find("input").at(0);
     });
 
     afterEach(() => {
         jest.clearAllMocks();
     });
 
-
     it('should render a <div />', () => {
         expect(wrapper.find('div').length).toEqual(1);
     });
 
-    describe("input Text", () => {
-        it("Should capture input value correctly onChange", () => {
-            const inputValue = wrapper.find("input").at(0);
-            inputValue.simulate('change', {
-                target: {
-                    value: 'Test'
-                }
-            });
-            expect(setState).toHaveBeenCalledWith("Test");
+    it("Should capture input value correctly onChange", () => {
+
+        inputValue.simulate('change', {
+            target: {
+                value: 'Test'
+            }
         });
+        expect(setState).toHaveBeenCalledWith("Test");
+    });
+
+    it('renders a Input with id of "id1" and placeholder message of "Enter your userName" and  title message of "User One"', () => {
+        const label = wrapper.find("label").at(0);
+        expect(inputValue.prop('id')).toBe('id1');
+        expect(inputValue.prop("placeholder")).toBe('Enter your userName');
+        expect(label.text()).toBe('User One');
     });
 
 });
