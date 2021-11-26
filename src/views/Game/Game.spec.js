78f9b6b4a7282fe913bfd5d41d588d4a07ed7b96
2021dev1122 <2021dev1122@email>
2021-11-26 12:02:25 +0100

    [19][Game] Create Test : "should render Board component"

--- src/views/Game/Game.spec.js
+++ src/views/Game/Game.spec.js
@@ -1,6 +1,6 @@
-import * as ReactRouter from 'react-router';
 import {shallow} from "enzyme";
 import Game from "./Game";
+import React from "react";
 
 
 jest.mock("react-router-dom", () => ({
@@ -11,8 +11,19 @@ jest.mock("react-router-dom", () => ({
     })
 }));
 describe("<Game />", () => {
+    let wrapper;
+    beforeEach(() => {
+
+        wrapper = shallow(<Game/>);
+    });
+
+    afterEach(() => {
+        jest.clearAllMocks();
+    });
+
+
     it("should render Game with data coming from Home", () => {
-        const wrapper = shallow(<Game/>);
+
         const h1 = wrapper.find("h1");
         expect(h1.at(0).text()).toBe("player1 : X")
         expect(h1.at(1).text()).toBe("player2 : O")
@@ -20,11 +31,16 @@ describe("<Game />", () => {
     });
 
     it('should render two div with className "col-md-6" inside div with className "row" ', () => {
-        const wrapper = shallow(<Game/>);
+
         const div = wrapper.find("div");
         expect(div.at(0).prop("className")).toBe("container")
         expect(div.at(1).prop("className")).toBe("row")
         expect(div.at(2).prop("className")).toBe("col-md-6")
         expect(div.at(3).prop("className")).toBe("col-md-6")
+    });
+
+    it('should render Board component ', () => {
+        const div = wrapper.find("Board");
+        expect(div.at(0).length).toEqual(1);
     });
 });
