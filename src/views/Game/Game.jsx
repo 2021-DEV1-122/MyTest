a89ee72f491c615d135adb8ffbcf1b844bd25c47
2021dev1122 <2021dev1122@email>
2021-11-26 12:02:38 +0100

    [19][Game] Fix Test : "should render Board component"

--- src/views/Game/Game.jsx
+++ src/views/Game/Game.jsx
@@ -1,12 +1,13 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {useLocation} from "react-router-dom";
+import Board from "../../components/Board/Board";
 
 const Game = (props) => {
 
     const location = useLocation()
     const {player1} = location.state
     const {player2} = location.state
-
+    const [board, setBoard] = useState(Array(9).fill(""));
     return (
         <div className="container">
             <div className="row">
@@ -16,6 +17,12 @@ const Game = (props) => {
                 <div className="col-md-6">
                     <h1>{player2} : O</h1>
                 </div>
+            </div>
+            <div className="row">
+                <div className="col-md-10">
+                    <Board value={board}></Board>
+                </div>
+
             </div>
         </div>
     );
