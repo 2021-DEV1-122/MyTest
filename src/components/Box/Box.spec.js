1fb4ed0d853d8c13ef1e54511d044d09bcb0724e
2021dev1122 <2021dev1122@email>
2021-11-26 12:33:52 +0100

    [20][Box] Create Test "return pos when click button"

--- src/components/Box/Box.spec.js
+++ src/components/Box/Box.spec.js
@@ -16,10 +16,19 @@ describe('<Box {...props} id="id1" value="X" />', () => {
     let wrapper;
     const setState = jest.fn();
     let button;
+    function MySpy() {
+        this.calls = 0;
+    }
+
+    MySpy.prototype.fn = function () {
+        return () => this.calls++;
+    }
+    const mySpy = new MySpy();
+    const mockCallBack = mySpy.fn();
 
     beforeEach(() => {
         useStateMock.mockImplementation(init => [init, setState]);
-        wrapper = shallow(<Box {...props} id="id1" value="X" />)
+        wrapper = shallow(<Box {...props}  onClick={mockCallBack} id="id1" value="X" />)
         button = wrapper.find("button").at(0);
     });
 
@@ -33,4 +42,8 @@ describe('<Box {...props} id="id1" value="X" />', () => {
         expect(button.text()).toBe('X');
     });
 
+    it('return pos when click button', () => {
+        button.find('button').simulate('click');
+        expect(mySpy.calls).toEqual(1);
+    });
 });
