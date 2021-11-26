9b4e3dc275dd379533a75fa231f1e8819b750ed6
2021dev1122 <2021dev1122@email>
2021-11-26 10:34:54 +0100

    [15][Game] Create Test : should render two div with className "col-md-6" inside div with className "row"

--- src/views/Game/Game.spec.js
+++ src/views/Game/Game.spec.js
@@ -12,11 +12,19 @@ jest.mock("react-router-dom", () => ({
 }));
 describe("<Game />", () => {
     it("should render Game with data coming from Home", () => {
-
         const wrapper = shallow(<Game/>);
 
         expect(wrapper.find("h1").at(0).text()).toBe("player1")
         expect(wrapper.find("h1").at(1).text()).toBe("player2")
 
+    });
+
+    it('should render two div with className "col-md-6" inside div with className "row" ', () => {
+        const wrapper = shallow(<Game/>);
+        const div = wrapper.find("div");
+        expect(div.at(0).prop("className")).toBe("container")
+        expect(div.at(1).prop("className")).toBe("row")
+        expect(div.at(2).prop("className")).toBe("col-md-6")
+        expect(div.at(3).prop("className")).toBe("col-md-6")
     });
 });
