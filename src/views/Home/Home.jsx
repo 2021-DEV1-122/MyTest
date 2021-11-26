6e61375a70024cbae83c729a67f8efca92a4cec1
2021dev1122 <2021dev1122@email>
2021-11-26 11:56:10 +0100

    [18][Home] Make Home Page interactive

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,17 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 import InputText from "../../components/InputText/InputText";
 import {Link} from "react-router-dom";
 
 const Home = () => {
-    // your link creation
+    const [userName1, setUserName1] = useState();
+    const [userName2, setUserName2] = useState();
 
 
     return (
         <div class="col-md-4 col-md-offset-4">
             <form>
-                <InputText id="userName1" placeholder="Enter your userName" title="Player One"></InputText>
-                <InputText id="userName2" placeholder="Enter your userName" title="Player Two"></InputText>
-                <Link to='/game'  state={{ player1: 'occupation' ,player2: '5555' }}  class="btn btn-info" role="button"> te</Link>
+                <InputText id="userName1" value={(data) => setUserName1(data)} placeholder="Enter your userName"
+                           title="Player One"></InputText>
+                <InputText id="userName2" value={(data) => setUserName2(data)} placeholder="Enter your userName"
+                           title="Player Two"></InputText>
+                <Link to='/game' state={{player1: userName1, player2: userName2}} class="btn btn-info"
+                      role="button"> Start the Game</Link>
             </form>
         </div>
     );
