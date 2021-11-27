2b27ea6acb197b506a58029ca4ec894b1486e8a4
2021dev1122 <2021dev1122@email>
2021-11-27 20:34:54 +0100

    [22] Fix Warning: Invalid DOM property `class`

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -8,13 +8,13 @@ const Home = () => {
 
 
     return (
-        <div class="col-md-4 col-md-offset-4">
+        <div className="col-md-4 col-md-offset-4">
             <form>
                 <InputText id="userName1" value={(data) => setUserName1(data)} placeholder="Enter your userName"
                            title="Player One"></InputText>
                 <InputText id="userName2" value={(data) => setUserName2(data)} placeholder="Enter your userName"
                            title="Player Two"></InputText>
-                <Link to='/game' state={{player1: userName1, player2: userName2}} class="btn btn-info"
+                <Link to='/game' state={{player1: userName1, player2: userName2}} className="btn btn-info"
                       role="button"> Start the Game</Link>
             </form>
         </div>
